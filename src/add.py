99ae74ef06f7c64bef66bd3059743fd7777d584b
yudai0731 <yudai40018@gmail.com>
2024-03-07 16:56:26 +0900

    add add-module and test

---
+++ src/add.py
@@ -0,0 +1,11 @@
+def add(number1: int, number2: int) -> int:
+    """number1とnumber2の加算
+
+    Args:
+        number1 (int): 整数
+        number2 (int): 整数
+
+    Returns:
+        int: number1+number2
+    """
+    return number1 + number2
